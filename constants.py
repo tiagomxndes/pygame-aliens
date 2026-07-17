77bd3a897b7c996d208e7f6f99ae651f011dbb13
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-07-17 20:41:33 +0100

    Added player spaceship rendering

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+PLAYER_RADIUS = 20  # the radius of the player's ship
+LINE_WIDTH = 2  # the width of the lines that draw the player's ship
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,6 +13,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -20,7 +22,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # draw objects here
+        player.draw(screen)
+
+        # show everything that was drawn
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
         print(dt)
 
