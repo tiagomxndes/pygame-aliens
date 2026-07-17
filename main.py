a490839d28cf09f08dddc2b1c8743f829d78b26b
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-07-17 20:05:11 +0100

    Limit game loop to 60FPS

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-
 print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
@@ -10,6 +9,8 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -20,6 +21,8 @@ def main():
                 return
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
